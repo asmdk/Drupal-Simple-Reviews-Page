121e0559b7cb903f33430ee6c4b014b3e10cadd0
Dzianis_Kel <Dzianis_Kel@epam.com>
2016-09-09 17:49:32 +0300

    init

--- simple-reviews-page-reviews-listing.tpl.php
+++ simple-reviews-page-reviews-listing.tpl.php
@@ -1,17 +1,26 @@
 <div id="simple-reviews-page">
+    <div class="leave-feedback"><a href="<?php echo url( 'reviews/send'); ?>"><?php echo t('Leave feedback') ;?></a></div>
     <?php if (!empty($reviews)) : ?>
-        <?php foreach ($reviews as $review) : ?>
-            <div class="review-content">
+        <?php foreach ($reviews as $index => $review) : ?>
+            <div class="review-content<?php if ($index == 0) echo ' first'; if ($index == (count($reviews) - 1)) echo ' last'; ?>">
                 <div class="review">
-                    <div><?php echo $review->name; ?><?php echo format_date($review->created); ?></div>
-                    <div><?php echo $review->topic; ?></div>
-                    <div><?php echo $review->message; ?></div>
+                    <div class="question-name">
+                        <?php echo $review->name; ?> <span><?php echo format_date($review->created, 'short'); ?></span>
+                    </div>
+                    <h3 class="question-topic"><?php echo $review->topic; ?></h3>
+                    <div class="question-message"><?php echo $review->message; ?></div>
                 </div>
-                <blockquote class="admin-comment">
-                    <div><?php echo $review->c_message; ?></div>
-                    <div><?php echo $review->c_name; ?><?php echo format_date($review->c_changed); ?></div>
-                </blockquote>
+                <?php if (!empty($review->c_message)) : ?>
+                    <div class="admin-comment">
+                        <div class="answer-name">
+                            <?php echo $review->c_name; ?>
+                            <span>(<?php echo $review->c_office; ?>)</span>:
+                        </div>
+                        <div class="answer-message"><?php echo $review->c_message; ?></div>
+                    </div>
+                <?php endif; ?>
             </div>
         <?php endforeach; ?>
+        <div class="leave-feedback"><a href="<?php echo url( 'reviews/send'); ?>"><?php echo t('Leave feedback') ;?></a></div>
     <?php endif; ?>
 </div>
